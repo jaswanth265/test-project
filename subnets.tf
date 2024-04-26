6b7f52156efbef689d6e244cc25691291e063bd9
jaswanth265 <k.jaswanth265@gmail.com>
2024-04-26 13:15:16 +0100

    modified code

--- subnets.tf
+++ subnets.tf
@@ -2,9 +2,10 @@
 
 
 resource "aws_subnet" "public_subnet1" {
-  vpc_id            = aws_vpc.web-vpc.id
-  cidr_block        = var.public_subnet1_cidr
-  availability_zone = "us-west-2a"
+  vpc_id                  = aws_vpc.web-vpc.id
+  cidr_block              = var.public_subnet1_cidr
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.public_subnet1_name}"
@@ -12,9 +13,10 @@ resource "aws_subnet" "public_subnet1" {
 }
 
 resource "aws_subnet" "public_subnet2" {
-  vpc_id            = aws_vpc.web-vpc.id
-  cidr_block        = var.public_subnet2_cidr
-  availability_zone = "us-west-2b"
+  vpc_id                  = aws_vpc.web-vpc.id
+  cidr_block              = var.public_subnet2_cidr
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.public_subnet2_name}"
@@ -25,6 +27,7 @@ resource "aws_subnet" "private_subnet1" {
   vpc_id            = aws_vpc.web-vpc.id
   cidr_block        = var.private_subnet1_cidr
   availability_zone = "us-west-2a"
+
 
   tags = {
     Name = "${var.private_subnet1_name}"
