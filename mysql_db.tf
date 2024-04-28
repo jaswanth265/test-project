ed560270e6aa5ea10d24fcb0b6d658134f534139
jaswanth265 <k.jaswanth265@gmail.com>
2024-04-28 14:28:13 +0100

    secrets manager for rds

--- mysql_db.tf
+++ mysql_db.tf
@@ -1,3 +1,17 @@
+data "aws_secretsmanager_secret" "example" {
+  name = "rds_admin6"
+  depends_on = [
+    aws_secretsmanager_secret.example
+  ]
+}
+
+data "aws_secretsmanager_secret_version" "secret" {
+  secret_id = data.aws_secretsmanager_secret.example.id
+}
+
+
+# DB Subnet Groups 
+
 resource "aws_db_subnet_group" "my_db_subnet_group" {
   # count      = length(aws_subnet.private_subnets)
   subnet_ids = [for subnet in aws_subnet.private_subnets : subnet.id]
@@ -10,18 +24,23 @@ resource "aws_db_subnet_group" "my_db_subnet_group" {
 
 # Create a RDS MySQL Instance
 resource "aws_db_instance" "mydb" {
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "8.0.36"
-  instance_class         = "db.t3.micro"
-  db_name                = "mydatabase"
-  username               = "admin"
-  password               = "yourpassword"
-  publicly_accessible    = false
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.rds_mysql_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.name
-  #db_subnet_group_name   = aws_db_subnet_group.my_db_subnet_group.id
-}
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  engine                  = "mysql"
+  engine_version          = "8.0.36"
+  instance_class          = "db.t3.micro"
+  db_name                 = "mydatabase"
+  publicly_accessible     = true
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.rds_mysql_sg.id]
+  db_subnet_group_name    = aws_db_subnet_group.my_db_subnet_group.name
+  password                = data.aws_secretsmanager_secret_version.secret.secret_string
+  backup_retention_period = 0
 
+  #db_subnet_group_name  = aws_db_subnet_group.my_db_subnet_group.id
+  #username              = "admin"
+  #assword               = "yourpassword"
+
+
+
+}
---
+++ secrets_manager.tf
@@ -0,0 +1,23 @@
+#Secrets Manager
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_!%^"
+}
+
+resource "aws_secretsmanager_secret" "example" {
+  kms_key_id              = aws_kms_key.default.key_id
+  name                    = "rds_admin6"
+  description             = "RDS Admin password"
+  recovery_window_in_days = 14
+
+  tags = {
+    Name = "Secrets-Manager"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "secret" {
+  secret_id     = aws_secretsmanager_secret.example.id
+  secret_string = random_password.password.result
+}
