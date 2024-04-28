59c6cc275fc4228be35aa6f7a3f9aef9597834e6
jaswanth265 <k.jaswanth265@gmail.com>
2024-04-28 05:30:25 +0100

    modifications

--- asg.tf
+++ asg.tf
@@ -9,7 +9,8 @@ resource "aws_autoscaling_group" "default_asg" {
   target_group_arns    = ["${aws_lb_target_group.default-tg.arn}"]
   health_check_type    = "EC2"
   launch_configuration = aws_launch_configuration.webserver-launch-config.name
-  vpc_zone_identifier  = var.vpc_zone_identifier_cidrs
+  vpc_zone_identifier  = [for subnet in aws_subnet.public_subnets : subnet.id]
+  # vpc_zone_identifier  = var.vpc_zone_identifier_cidrs
   #vpc_zone_identifier  = ["${aws_subnet.public_subnet1.id}", "${aws_subnet.public_subnet2.id}"]
 
   tag {
